524cba8aa0effdadd2f650b49e7fff262de165fe
Matt McManus <mattmcmanus@gmail.com>
2011-11-15 14:08:31 -0500

    Variable cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,18 @@
 class duplicity {
   # Install the package
-  package {   
+  package {
     ['duplicity', 'python-boto']: ensure => present
   }
-  
+
   if $enable_backup {
     if !$s3_bucket {
       fail("You need to define a bucket name!")
     }
-    
+
     if (!$aws_access_key_id and !$aws_secret_access_key and !$passphrase) {
       fail("You need to set all of your key variables: aws_access_key_id, aws_secret_access_key and passphrase")
     }
-    
+
     file {
       "file-backup.sh":
         path => "/root/scripts/file-backup.sh",
@@ -21,7 +21,7 @@ class duplicity {
         owner => root, group => 0, mode => 0500,
         ensure => present;
     }
-    
+
     cron { 'duplicity_backup_cron':
       command => "/bin/sh /root/scripts/file-backup.sh",
       user => 'root',
