46b11514a11d38055f454b1757c0327dbed8daeb
Soenke Ruempler <soenke@ruempler.eu>
2012-08-02 22:16:05 +0200

    do not set a default bucket, makes no sense

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class duplicity(
   $directories,
-  $bucket = $duplicity::params::bucket,
+  $bucket,
   $dest_id = $duplicity::params::dest_id,
   $dest_key = $duplicity::params::dest_key,
   $folder = $duplicity::params::folder,
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,8 @@
 class duplicity::params inherits duplicity::defaults {
-  $bucket = $::fqdn # XXX need to find some good default, or none
   $folder = $::fqdn
   $dest_id = $::duplicity_dest_id
   $dest_key = $::duplicity_dest_key
   $cloud = 's3'
+  $bucket = undef
   $pubkey_id = undef
 }
