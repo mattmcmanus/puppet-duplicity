cd4db6f0ef7e695909ad2a64fa4b0be62d4bcf0f
Soenke Ruempler <soenke@ruempler.eu>
2012-08-02 17:33:11 +0200

    specs

--- spec/classes/duplicity_spec.rb
+++ spec/classes/duplicity_spec.rb
@@ -19,11 +19,58 @@ describe 'duplicity' do
     }
   }
 
-  it { should contain_package('duplicity') }
+  it "contains package" do
+    should contain_package('duplicity')
+    should contain_package('gnupg')
+  end
 
-  it {
+  it "generates a file backup script to /usr/local/bin" do
     should contain_file('file-backup.sh') \
       .with_path('/usr/local/bin/duplicity_backup_puppet.sh') \
       .with_content(/duplicity --full-if-older-than 30D --s3-use-new-style --no-encryption --include \'\/etc\/\' --exclude \'\*\*\' \/ s3\+http:\/\/somebucket\/#{Regexp.escape(fqdn)}/)
+  end
+
+  it "should make a full backup every X days" do
+
+  end
+
+  it "should be able to backup more than one directory" do
+  end
+
+end
+
+describe 'duplicity with pubkey encryption' do
+
+  some_pubkey_id = '15ABDA79'
+
+  let(:params) {
+    {
+      :bucket       => 'somebucket',
+      :directories    => [ '/etc/' ],
+      :dest_id  => 'some_id',
+      :dest_key => 'some_key',
+      :pubkey   => some_pubkey_id
+    }
   }
+
+  it "should use pubkey encryption if keyid is provided" do
+    should contain_file('file-backup.sh') \
+      .with_content(/duplicity --full-if-older-than 30D --s3-use-new-style --encrypt-key #{some_pubkey_id} --include \'\/etc\/\' --exclude \'\*\*\' \/ s3\+http:\/\/somebucket\/#{Regexp.escape(fqdn)}/)
+  end
+
+  it "should download and import the specified pubkey" do
+    should contain_exec('duplicity-pgp') \
+      .with_command("gpg --keyserver subkeys.pgp.net --recv-keys #{some_pubkey_id}") \
+      .with_path("/usr/bin:/usr/sbin:/bin") \
+      .with_unless("gpg --list-key #{some_pubkey_id}")
+  end
+end
+
+describe 'duplicity defaults' do
+
+  it "should be able to set a global cloud key pair config" do
+  end
+
+  it "should be able to set a global pubkey id" do
+  end
 end
