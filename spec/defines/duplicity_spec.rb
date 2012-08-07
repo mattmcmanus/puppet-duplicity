89da7b4e04eee68ccfd83b6ae35400ce1eb543b8
Soenke Ruempler <soenke@ruempler.eu>
2012-08-07 17:10:12 +0200

    use defined resource name as unique folder in bucket

--- spec/defines/duplicity_spec.rb
+++ spec/defines/duplicity_spec.rb
@@ -30,7 +30,7 @@ describe 'duplicity', :type => :define do
 
     it "adds a cronjob at midnight be default" do
       should contain_cron('some_backup_name') \
-        .with_command("duplicity --full-if-older-than 30D --s3-use-new-style --no-encryption --include '/etc/' --exclude '**' / 's3\+http://somebucket/#{fqdn}'") \
+        .with_command("duplicity --full-if-older-than 30D --s3-use-new-style --no-encryption --include '/etc/' --exclude '**' / 's3\+http://somebucket/#{fqdn}/some_backup_name/'") \
         .with_user('root') \
         .with_minute(0) \
         .with_hour(0) \
@@ -43,7 +43,7 @@ describe 'duplicity', :type => :define do
     end
   end
 
-  context "defined backup zime" do
+  context "with defined backup time" do
 
     let(:params) {
       {
