0a60fbb1118c542001879c2f91a8b78bf85d50eb
Ingo Oeser <ingo@jimdo.com>
2013-07-15 23:36:45 +0200

    Fully implement monitored duplicity job

--- manifests/monitored_job.pp
+++ manifests/monitored_job.pp
@@ -1,14 +1,39 @@
 define duplicity::monitored_job(
-  $bucket,
-  $directory,
-  $dest_id,
-  $dest_key,
-  $execution_timeout,
-  $minute,
-  $hour,
+  $ensure = 'present',
+  $directory = undef,
+  $bucket = undef,
+  $dest_id = undef,
+  $dest_key = undef,
+  $folder = undef,
+  $cloud = undef,
+  $pubkey_id = undef,
+  $hour = undef,
+  $minute = undef,
+  $full_if_older_than = undef,
+  $pre_command = undef,
+  $remove_older_than = undef,
+  $execution_timeout
 )
 {
   include duplicity::params
+  include duplicity::packages
+
+  $spoolfile = "${duplicity::params::job_spool}/${name}.sh"
+
+  duplicity::job { $name :
+    ensure => $ensure,
+    spoolfile => $spoolfile,
+    directory => $directory,
+    bucket => $bucket,
+    dest_id => $dest_id,
+    dest_key => $dest_key,
+    folder => $folder,
+    cloud => $cloud,
+    pubkey_id => $pubkey_id,
+    full_if_older_than => $full_if_older_than,
+    pre_command => $pre_command,
+    remove_older_than => $remove_older_than,
+  }
 
   $_hour = $hour ? {
     undef => $duplicity::params::hour,
@@ -20,13 +45,39 @@ define duplicity::monitored_job(
     default => $minute
   }
 
-  periodicnoise::monitored_cron { $title :
-    command => "",
+  $_dest_id = $dest_id ? {
+    undef => $duplicity::params::dest_id,
+    default => $dest_id
+  }
+
+  $_dest_key = $dest_key ? {
+    undef => $duplicity::params::dest_key,
+    default => $dest_key
+  }
+
+  $_cloud = $cloud ? {
+    undef => $duplicity::params::cloud,
+    default => $cloud
+  }
+
+  $environment = $_cloud ? {
+    'cf' => ["CLOUDFILES_USERNAME='$_dest_id'", "CLOUDFILES_APIKEY='$_dest_key'"],
+    's3' => ["AWS_ACCESS_KEY_ID='$_dest_id'", "AWS_SECRET_ACCESS_KEY='$_dest_key'"],
+  }
+
+
+  Cron {
+    environment => $environment,
+  }
+
+  periodicnoise::monitored_cron { $name :
+    ensure => $ensure,
+    command => $spoolfile,
     user => 'root',
     minute => $_minute,
     hour => $_hour,
-    execution_timeout => $execution_timeout
-
+    execution_timeout => $execution_timeout,
   }
 
+  File[$spoolfile]->Cron[$name]
 }
--- spec/defines/monitored_job_spec.rb
+++ spec/defines/monitored_job_spec.rb
@@ -1,6 +1,6 @@
-describe 'duplicity::monitored_job' do
+require 'spec_helper'
 
-  let (:title) { 'some_descriptive_name_for_duplicity_job' }
+describe 'duplicity::monitored_job' do
 
   let (:params) { {
     :bucket            => 'somebucket',
@@ -10,17 +10,279 @@ describe 'duplicity::monitored_job' do
     :minute            => 0,
     :hour              => 0,
     :execution_timeout => '24h',
+    :cloud             => 's3',
   } }
+
+  fqdn = 'somehost.domaindomain.org'
+
+  let(:facts) {
+    {
+      :fqdn => fqdn,
+    }
+  }
+
+  let(:title) { 'some_backup_name' }
+
+  let(:spoolfile) { "/var/spool/duplicity/#{title}.sh" }
 
   it 'should monitor the duplicity backup job with periodic noise' do
     should contain_periodicnoise__monitored_cron(title).with({
-        :command => "",
-        :user    => 'root',
-        :minute  => 0,
-        :hour    => 0,
+      :command => spoolfile,
+      :user    => 'root',
+      :minute  => 0,
+      :hour    => 0,
+      :execution_timeout => '24h',
     })
+  end
 
-#    should contain_duplicity(title)
+
+  context "cloud files environment" do
+
+    let(:params) {
+      {
+        :bucket       => 'somebucket',
+        :directory    => '/etc/',
+        :dest_id  => 'some_id',
+        :dest_key => 'some_key',
+        :execution_timeout => '24h',
+        :cloud    => 'cf'
+      }
+    }
+
+    it "adds a cronjob at midnight by default" do
+
+      should contain_periodicnoise__monitored_cron(title) \
+        .with_command(spoolfile)
+
+      should contain_cron(title) \
+        .with_environment([ 'CLOUDFILES_USERNAME=\'some_id\'', 'CLOUDFILES_APIKEY=\'some_key\'' ])
+
+      should contain_file(spoolfile) \
+        .with_content(/^duplicity --full-if-older-than 30D --s3-use-new-style --no-encryption --include '\/etc\/' --exclude '\*\*' \/ 'cf\+http:\/\/somebucket'$/)
+    end
+  end
+
+  context "without encryption" do
+
+    let(:params) {
+      {
+        :bucket       => 'somebucket',
+        :directory    => '/etc/',
+        :dest_id  => 'some_id',
+        :dest_key => 'some_key',
+        :execution_timeout => '24h',
+      }
+    }
+
+    it "adds a cronjob at midnight by default" do
+      should contain_periodicnoise__monitored_cron(title) \
+        .with_command(spoolfile)
+
+      should contain_cron(title) \
+        .with_environment([ 'AWS_ACCESS_KEY_ID=\'some_id\'', 'AWS_SECRET_ACCESS_KEY=\'some_key\'' ])
+
+      should contain_file(spoolfile) \
+        .with_content(/^duplicity --full-if-older-than 30D --s3-use-new-style --no-encryption --include '\/etc\/' --exclude '\*\*' \/ 's3\+http:\/\/somebucket\/#{fqdn}\/some_backup_name\/'$/)
+    end
+  end
+
+  context "with defined force full-backup" do
+
+    let(:params) {
+      {
+        :bucket             => 'somebucket',
+        :directory          => '/etc/',
+        :dest_id            => 'some_id',
+        :execution_timeout => '24h',
+        :dest_key           => 'some_key',
+        :full_if_older_than => '5D',
+      }
+    }
+
+    it "should be able to handle a specified backup time" do
+      should contain_periodicnoise__monitored_cron(title) \
+        .with_command(spoolfile)
+
+      should contain_file(spoolfile) \
+        .with_content(/--full-if-older-than 5D/)
+    end
+  end
+
+  context "with defined backup time" do
+
+    let(:params) {
+      {
+        :bucket       => 'somebucket',
+        :directory    => '/etc/',
+        :dest_id      => 'some_id',
+        :execution_timeout => '24h',
+        :dest_key     => 'some_key',
+        :hour         => 5,
+        :minute       => 23
+      }
+    }
+
+    it "should be able to handle a specified backup time" do
+       should contain_periodicnoise__monitored_cron(title) \
+         .with_minute(23) \
+         .with_hour(5)
+    end
+  end
+
+  context "with defined remove-older-than" do
+
+    let(:params) {
+      {
+        :bucket             => 'somebucket',
+        :execution_timeout => '24h',
+        :directory          => '/etc/',
+        :dest_id            => 'some_id',
+        :dest_key           => 'some_key',
+        :remove_older_than => '7D',
+      }
+    }
+
+    it "should be able to handle a specified remove-older-than time" do
+      should contain_periodicnoise__monitored_cron(title) \
+        .with_command(spoolfile)
+
+      should contain_file(spoolfile) \
+        .with_content(/remove-older-than 7D.* --no-encryption --force.*/)
+    end
+  end
+
+  context 'duplicity with pubkey encryption' do
+
+    some_pubkey_id = '15ABDA79'
+    fqdn = 'somehost.domaindomain.org'
+
+    let(:params) {
+      {
+        :bucket       => 'somebucket',
+        :execution_timeout => '24h',
+        :directory    => '/etc/',
+        :dest_id  => 'some_id',
+        :dest_key => 'some_key',
+        :pubkey_id   => some_pubkey_id
+      }
+    }
+
+    it "should use pubkey encryption if keyid is provided" do
+      should contain_periodicnoise__monitored_cron(title) \
+        .with_command(spoolfile)
+
+      should contain_file(spoolfile) \
+        .with_content(/--encrypt-key #{some_pubkey_id}/)
+    end
+
+    it "should download and import the specified pubkey" do
+      should contain_exec('duplicity-pgp') \
+        .with_command("gpg --keyserver subkeys.pgp.net --recv-keys #{some_pubkey_id}") \
+        .with_path("/usr/bin:/usr/sbin:/bin") \
+        .with_unless("gpg --list-key #{some_pubkey_id}")
+    end
+  end
+
+  context 'with default bucket and bucket as param' do
+    let(:params) {
+      {
+        :directory    => '/etc/',
+        :execution_timeout => '24h',
+        :bucket       => 'from_param'
+      }
+    }
+
+    let (:pre_condition) {
+      "class { 'duplicity::params' :
+        bucket => 'default',
+        dest_id => 'some_id',
+        dest_key => 'some_key'
+      }"
+    }
+
+    it "should override default bucket with param" do
+      should contain_periodicnoise__monitored_cron(title) \
+        .with_command(spoolfile)
+
+      should contain_file(spoolfile) \
+        .with_content(/from_param/)
+    end
+  end
+
+  context 'duplicity defaults' do
+    let(:params) {
+      {
+        :execution_timeout => '24h',
+        :directory    => '/etc/',
+      }
+    }
+
+    let (:pre_condition) {
+      "class { 'duplicity::params' :
+        bucket => 'another_bucket',
+        dest_id => 'some_id',
+        dest_key => 'some_key'
+      }"
+    }
+
+    it "contains package" do
+      should contain_package('duplicity')
+      should contain_package('gnupg')
+    end
+
+    it "should be able to set a global cloud key pair config" do
+      should contain_periodicnoise__monitored_cron(title) \
+        .with_command(spoolfile)
+
+      should contain_cron(title) \
+        .with_environment([ 'AWS_ACCESS_KEY_ID=\'some_id\'', 'AWS_SECRET_ACCESS_KEY=\'some_key\'' ])
+
+      should contain_file(spoolfile) \
+        .with_content(/another_bucket/)
+
+    end
+
+    it "should be able to set a global pubkey id" do
+    end
+  end
+
+  context "with pre_command" do
+
+    let(:params) {
+      {
+        :bucket       => 'somebucket',
+        :directory    => '/root/mysqldump',
+        :dest_id      => 'some_id',
+        :execution_timeout => '24h',
+        :dest_key     => 'some_key',
+        :pre_command  => 'mysqldump database',
+      }
+    }
+
+    it "should prepend pre_command to cronjob" do
+      should contain_periodicnoise__monitored_cron(title) \
+        .with_command(spoolfile)
+
+      should contain_file(spoolfile) \
+        .with_content(/^mysqldump database && /)
+    end
+  end
+
+  context 'with ensure => absent' do
+
+    let(:params) {
+      {
+        :execution_timeout => '24h',
+        :ensure       => 'absent'
+      }
+    }
+
+    it 'should remove the cron and the job file' do
+      should contain_periodicnoise__monitored_cron(title) \
+        .with_ensure('absent')
+      should contain_file(spoolfile) \
+        .with_ensure('absent')
+    end
   end
 
 end
