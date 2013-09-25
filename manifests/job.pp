ecfc12f15af9387e74e755bbe466a4ca0439101e
Ryan Brooks <ryanbrooksis@gmail.com>
2013-09-25 14:45:38 +0100

    Recovering from puppet lint corrections, where I introduced a few new errors.

--- manifests/job.pp
+++ manifests/job.pp
@@ -19,6 +19,51 @@ define duplicity::job(
   include duplicity::params
   include duplicity::packages
 
+  $_bucket = $bucket ? {
+    undef => $duplicity::params::bucket,
+    default => $bucket
+  }
+
+  $_dest_id = $dest_id ? {
+    undef => $duplicity::params::dest_id,
+    default => $dest_id
+  }
+
+  $_dest_key = $dest_key ? {
+    undef => $duplicity::params::dest_key,
+    default => $dest_key
+  }
+
+  $_folder = $folder ? {
+    undef => $duplicity::params::folder,
+    default => $folder
+  }
+
+  $_cloud = $cloud ? {
+    undef => $duplicity::params::cloud,
+    default => $cloud
+  }
+
+  $_pubkey_id = $pubkey_id ? {
+    undef => $duplicity::params::pubkey_id,
+    default => $pubkey_id
+  }
+
+  $_hour = $hour ? {
+    undef => $duplicity::params::hour,
+    default => $hour
+  }
+
+  $_minute = $minute ? {
+    undef => $duplicity::params::minute,
+    default => $minute
+  }
+
+  $_full_if_older_than = $full_if_older_than ? {
+    undef => $duplicity::params::full_if_older_than,
+    default => $full_if_older_than
+  }
+
   $_pre_command = $pre_command ? {
     undef => '',
     default => "$pre_command && "
@@ -34,7 +79,7 @@ define duplicity::job(
     default => $remove_older_than,
   }
 
-  if !($cloud in [ 's3', 'cf' ]) {
+  if !($_cloud in [ 's3', 'cf' ]) {
     fail('$cloud required and at this time supports s3 for amazon s3 and cf for Rackspace cloud files')
   }
 
@@ -45,11 +90,11 @@ define duplicity::job(
         fail('directory parameter has to be passed if ensure != absent')
       }
 
-      if !$bucket {
+      if !$_bucket {
         fail('You need to define a container/bucket name!')
       }
 
-      if (!$dest_id or !$dest_key) {
+      if (!$_dest_id or !$_dest_key) {
         fail('You need to set all of your key variables: dest_id, dest_key')
       }
 
@@ -62,22 +107,22 @@ define duplicity::job(
     }
   }
 
-  $_cfhash = { 'CLOUDFILES_USERNAME' => $dest_id, 'CLOUDFILES_APIKEY'     => $dest_key,}
-  $_awshash = { 'AWS_ACCESS_KEY_ID'  => $dest_id, 'AWS_SECRET_ACCESS_KEY' => $dest_key,}
+  $_cfhash = { 'CLOUDFILES_USERNAME' => $_dest_id, 'CLOUDFILES_APIKEY'     => $_dest_key,}
+  $_awshash = { 'AWS_ACCESS_KEY_ID'  => $_dest_id, 'AWS_SECRET_ACCESS_KEY' => $_dest_key,}
 
-  $_environment = $cloud ? {
+  $_environment = $_cloud ? {
     'cf' => $_cfhash,
     's3' => $_awshash,
   }
 
-  $_target_url = $cloud ? {
-    'cf' => "'cf+http://${bucket}'",
-    's3' => "'s3+http://${bucket}/${folder}/${name}/'"
+  $_target_url = $_cloud ? {
+    'cf' => "'cf+http://$_bucket'",
+    's3' => "'s3+http://$_bucket/$_folder/$name/'"
   }
 
-  $_remove_older_than_command = $remove_older_than ? {
+  $_remove_older_than_command = $_remove_older_than ? {
     undef => '',
-    default => " && duplicity remove-older-than ${remove_older_than} --s3-use-new-style ${_encryption} --force ${_target_url}"
+    default => " && duplicity remove-older-than $_remove_older_than --s3-use-new-style $_encryption --force $_target_url"
   }
 
   file { $spoolfile:
